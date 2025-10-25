1c0d68524fd6389de63a21dc851ac0b76a3b2f59
Ziousss <johnsson_daniel@outlook.com>
2025-10-26 00:47:16 +0100

    some comments for the future and added the beginning of the function which handles the first start ever of the program

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <sodium.h>
 
 /* Definitions */
 #define MAX_SIZE 256
@@ -15,17 +16,24 @@ void add_pwd();
 /* Struct */
 typedef struct {
     char name[MAX_SIZE];
-    char pwd[MAX_SIZE];
+    char pwd[MAX_SIZE]; // will be encrypted w their nonce
+    // need to implement nonce
 } Record;
 
 /* Function's code */
+
+// need to change pwd form csv to wb for storage of salt and nonce
 int main(int argc, char* argv[]){
     if (argc != 1){
         return 1;
     }
-    int usage = 0;
     int myNum;
-
+    char row[100];
+    FILE *file = fopen("pwd.csv", "r");
+    if(fgets(row, 100, file) == NULL){
+        first_time();
+    }
+    fclose(file);
     printf("What do you want to do ?\n"); 
     printf("1. Add new password\n 2. See current password\n 3. Change existing password\n 4. Delete a password from the list\n");
     scanf("%d", &myNum); 
@@ -93,7 +101,9 @@ void add_pwd(){
     printf("For what is this password for ? [name]\n");
     scanf("%s", new_name);
     char new_pwd[50];
+
     //will add the option of generate a new password later
+
     printf("What is the password?\n");
     scanf("%s", new_pwd);
 
@@ -107,7 +117,45 @@ void add_pwd(){
     fclose(file);
 }
 
-/*
-things to implement
-    pwd evaluation; strong, medium, weak.
-*/
+void first_time(){
+    char pwd[30];
+    char conf_pwd[30];
+
+    printf("This is the first time you open this program.\n");
+    printf("Please set a password. This one will be asked every time you open the program so be sure to remember it!\n");
+    scanf("%s", pwd);
+
+    char* level = pwd_level();
+    do {
+        char* level = pwd_level();
+        printf("This password is %s. Please confirm by retyping it. Press 1 to change the original password: ", level);
+        scanf("%s", conf_pwd);
+
+        if(strcmp(conf_pwd,"1") == 0){
+            printf("Change orignial password: ");
+            scanf("%s", pwd);
+        }
+
+    } while (strcmp(pwd,conf_pwd) != 0);
+
+    if (sodium_init() < 0) {
+        return 1;
+    }
+    unsigned char salt[crypto_pwhash_SALTBYTES];
+    randombytes_buf(salt, sizeof(salt));
+
+    FILE *file = fopen("pwd.csv","a");
+    fprintf(file, "salt,");
+    for (int i = 0; i < crypto_pwhash_SALTBYTES; i++) {
+        fprintf(file, "%02x", salt[i]);
+    }
+    fprintf(file, "\n");
+    fclose(file);
+
+    // need to implement nonces
+    // needs to derive a key from pwd and salt
+    // still needs to encrypt a string like "verif_cypher" to try key when connecting
+
+
+    return;
+}
