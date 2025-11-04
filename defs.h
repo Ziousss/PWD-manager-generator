a9215ce3671dbc9c2e9526adaea799fec4b45d2b
Archie S <me@platinumxy.com>
2025-11-04 17:58:35 +0000

    fixed duplicates

--- defs.h
+++ defs.h
@@ -9,11 +9,5 @@
 #define NONCE_SIZE 24
 #define PWD_LENGTH 50
 #define HEADER_SIZE (SALT_SIZE + NONCE_SIZE + crypto_secretbox_MACBYTES + PWD_LENGTH)
-#define MAX_RECORD 256
-#define NAME_SIZE 50
-#define SALT_SIZE 16
-#define NONCE_SIZE 24
-#define PWD_LENGTH 50
-#define HEADER_SIZE (SALT_SIZE + NONCE_SIZE + crypto_secretbox_MACBYTES + PWD_LENGTH)
 
 #endif 
